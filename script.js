65e348234b87a40ae2c764a68025ce717552d685
sherrywilly <sherrywilson521@gmail.com>
2020-12-02 13:10:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const  app = express()
+const Uname = "sherrywilson521@gmail.com";
+const Upass = "sherry@123";
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(express.static('views'));
+app.post('/login',(req, res)=>{
+    console.log(req.body);
+    const {email,password} = req.body;
+    if(Uname === email && Upass === password){
+        res.send("user successfully loged in")
+    }else{
+            res.send("invalid user")
+    }
+
+})
+
+const PORT = process.env.PORT||3002;
+
+app.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`)
+})
